52be2c5b73a055aaa5ff04fe719b6fe1df838fd6
AG-code32 <i.gerson@hotmail.com>
2023-04-05 07:55:54 -0500

    adding logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './logo.css'
 
+
 const Logo = () => {
+  const [activeNav, setActiveNav] = useState('#');
+
   return (
-    <a className='logoGY' href="#">
+    <a href={activeNav} className='logoGY'>
+    {/* <a className='logoGY' href="#"> */}
       <img src="./logoGY-Port.svg" alt="logo gerson yance GY white svg" className='logoGY_img'/>
     </a>
   )
